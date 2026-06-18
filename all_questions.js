431ec54393adef64be9c2a6be39eb072018ad12b
kyokyo27 <11kyosuke27@gmail.com>
2026-06-19 02:49:38 +0900

    Add files via upload

---
+++ all_questions.js
@@ -0,0 +1,8 @@
+// all_questions.js
+
+// 1. 問題データを格納するための空の箱（配列）を準備する
+var EMBEDDED_QUESTIONS = [];
+
+// 2. 分割したJSファイルを強制的に順番通りに読み込ませる
+document.write('<script src="gyoseiho.js"></script>');
+// ファイルを増やしたい場合は、ここに document.write('<script src="ファイル名.js"></script>'); を追加していく
